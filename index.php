770f029d22bd2eeef80eb5ee11442b18a14f892d
minion6011 <mvittorio29@gmail.com>
2026-07-03 17:14:08 +0200

    Added index.php, added navbar.html and footer.html to the component folder

--- index.php
+++ index.php
@@ -11,24 +11,9 @@
     <script src="main.js" type="text/javascript" defer></script>
 </head>
 <body>
-    <div class="navbar">
-        <img src="assets/imgs/logo-full.svg" class="side logo">
-
-        <div class="center links">
-            <a class="active">Home</a>
-            <a>Esplora</a>
-            <a>Chi siamo</a>
-        </div>
-
-        <div class="side btns">
-            <button>
-                <size>+</size> 
-                Pubblica un mezzo
-            </button>
-            <i class="fa fa-bell-o"></i>
-            <img src="assets/imgs/add.svg" class="account">
-        </div>
-    </div>
+    <?php
+        include("components/navbar.html")
+    ?>
 
     <div class="content">
         <div class="presentation">
@@ -118,45 +103,8 @@
 
     <br><br>
 
-    <div class="footer">
-        <div class="infopoint">
-            <div class="info">
-                <iname>BoatSharing</iname>
-                <a href="test.com">Chi siamo</a>
-                <a href="test.com">La sostenibilità</a>
-                <a href="test.com">Il nostro Team</a>
-            </div>
-
-            <div class="info">
-                <iname>Scopri</iname>
-                <a href="test.com">Come funziona</a>
-                <a href="test.com">Crea un annuncio</a>
-                <a href="test.com">Pubblica un annuncio</a>
-                <a href="test.com">Registrati/Accedi</a>
-            </div>
-
-            <div class="info">
-                <iname>Assistenza</iname>
-                <a href="test.com">Centro assistenza</a>
-                <a href="test.com">Contattaci</a>
-            </div>
-        </div>
-
-        <hr>
-
-        <div class="socials">
-            <i class="fa fa-github fa-3" aria-hidden="true"></i>
-        </div>
-
-        <hr>
-
-        <div class="privacy-section">
-            <a>Informativa sui cookie</a>
-            <a>Termini e condizioni</a>
-            <a>Privacy Policy</a>
-            <a>Trattamento dati</a>
-        </div>
-
-    </div>
+    <?php
+        include("components/footer.html")
+    ?>
 </body>
 </html>
